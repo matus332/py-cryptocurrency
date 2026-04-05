33ef5b61a7dac10cf08f41798b77c326b3de4781
Denys Matusevych <denytop3@gmail.com>
2026-04-05 22:03:28 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,16 +4,16 @@ from typing import Union
 
 @patch("app.main.get_exchange_rate_prediction")
 def test_to_buy_more_crypto(mocked_rate: Union[int, float]) -> None:
-    mocked_rate.return_value = 4
+    mocked_rate.return_value = 1.1
 
-    assert cryptocurrency_action(2) == "Buy more cryptocurrency"
+    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
 def test_to_sell_crypto(mocked_rate: Union[int, float]) -> None:
-    mocked_rate.return_value = 1
+    mocked_rate.return_value = 0.9
 
-    assert cryptocurrency_action(2) == "Sell all your cryptocurrency"
+    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
